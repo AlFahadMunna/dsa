c3751f2623a669fd93457d3b456ca4830cc92f10
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-03-19 11:41:05 +0600

    sum of natural numbers

---
+++ loops/app.js
@@ -0,0 +1,22 @@
+// sum of n natural numbers
+
+// let pr = prompt("give your number");
+
+// if (pr === null) {
+//   console.log("cancelled");
+// } else {
+//   let ans = Number(pr);
+//   if (isNaN(ans)) {
+//     console.log("invalid input");
+//   } else {
+//     if (ans > 0) {
+//       var sum = 0;
+//       for (var i = 1; i <= ans; i++) {
+//         sum = sum + i;
+//       }
+//       console.log(sum);
+//     } else {
+//       console.log("should be positive ans more than 0");
+//     }
+//   }
+// }
