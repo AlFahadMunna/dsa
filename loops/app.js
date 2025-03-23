1b43a910c9cc51c30fd5a7c848b50c596531cb3c
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-03-23 11:43:51 +0600

    guessing the number

--- loops/app.js
+++ loops/app.js
@@ -179,3 +179,23 @@
 //     }
 //   }
 // }
+
+//guess the number
+
+// let random = Math.floor(Math.random(100) * 100) + 1;
+
+// let guess = 0;
+// while (guess != random) {
+//   guess = Number(prompt("Guess the number"));
+//   if (isNaN(guess) || guess < 1 || guess > 100) {
+//     console.log("try again b/w 1-100");
+//     continue;
+//   }
+//   if (guess > random) {
+//     console.log("too high , try again");
+//   } else if (guess < random) {
+//     console.log("too low, try again");
+//   } else {
+//     console.log("congrats and number was", guess);
+//   }
+// }
