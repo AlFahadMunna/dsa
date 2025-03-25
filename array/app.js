cbf29abb7fe945b3946ccdbced134a0755c6ee12
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-03-25 23:36:15 +0600

    move 0 to left

--- array/app.js
+++ array/app.js
@@ -46,3 +46,20 @@
 //   j--;
 // }
 // console.log(arr);
+
+// move all 0 to left and all 1 to right
+
+// let arr = [1, 0, 1, 0, 1, 0, 0, 1, 1];
+// let i = 0,
+//   j = 0;
+
+// while (i < arr.length) {
+//   if (arr[i] == 0) {
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     j++;
+//   }
+//   i++;
+// }
+// console.log(arr);
