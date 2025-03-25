98ff4e110424a4a348f37b443efdfbc44c29ef3d
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-03-25 17:49:33 +0600

    max and min number finder

---
+++ array/app.js
@@ -0,0 +1,17 @@
+// let arr = [44, 33, 5, 322, 564, 54];
+// let max = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
+
+// let arr = [44, 33, 5, 322, 564, 54];
+// let min = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+// }
+// console.log(min);
