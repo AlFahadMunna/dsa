ee144ea8e80ba7e9a91a1458ab068319a5d5bf0a
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-03-25 22:52:17 +0600

    find second max number

--- array/app.js
+++ array/app.js
@@ -15,3 +15,20 @@
 //   }
 // }
 // console.log(min);
+
+//find second max number
+
+// let arr = [44, 33, 5, 322, 564, 54];
+
+// let max = Math.max(arr[0], arr[1]);
+// let smax = Math.min(arr[0], arr[1]);
+
+// for (let i = 2; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     smax = max;
+//     max = arr[i];
+//   } else if (arr[i] > smax && max != arr[i]) {
+//     smax = arr[i];
+//   }
+// }
+// console.log(smax);
