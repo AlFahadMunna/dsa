a41c34acfdc0e9fce316df25efef8279b584ce71
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-05-25 15:29:03 +0600

    left rotation by 1 element

---
+++ advance_array/app.js
@@ -0,0 +1,12 @@
+// left rotation by 1 element
+
+// let arr =[1,2,3,4,5]
+
+// let first = arr[0]
+
+// for (let i=0;i<arr.length-1;i ++){
+//     arr[i]=arr[i+1]
+// }
+// arr[arr.length-1]= first
+
+// console.log(arr);
