0a7b35365786f23d039b8a202c4f3d54545c9c6f
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-05-25 15:53:20 +0600

    left and right rotation of an array

--- advance_array/app.js
+++ advance_array/app.js
@@ -1,24 +1,24 @@
-// left rotation by 1 element
+//left rotation by 1 element
 
-// let arr =[1,2,3,4,5]
+let arr =[1,2,3,4,5]
 
-// let first = arr[0]
+let first = arr[0]
 
-// for (let i=0;i<arr.length-1;i ++){
-//     arr[i]=arr[i+1]
-// }
-// arr[arr.length-1]= first
+for (let i=0;i<arr.length-1;i ++){
+    arr[i]=arr[i+1]
+}
+arr[arr.length-1]= first
 
-// console.log(arr);
+console.log(arr);
 
-// right rotation by 1 element
+//right rotation by 1 element
 
-// let arr =[1,2,3,4,5]
+let arr =[1,2,3,4,5]
 
-// let last = arr.length;
+let last = arr.length;
 
-// for(let i =arr.length-1;i>0;i--){
-//     arr[i]=arr[i-1]
-// }
-// arr[0]=last
-// console.log(arr);
+for(let i =arr.length-1;i>0;i--){
+    arr[i]=arr[i-1]
+}
+arr[0]=last
+console.log(arr);
