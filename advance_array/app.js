2b79bc00e6454c712f304ee421071e9070b55605
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-05-25 18:06:08 +0600

    left rotation 2 element best way

--- advance_array/app.js
+++ advance_array/app.js
@@ -34,3 +34,27 @@
 //     temp[i] = arr[(i+k)%arr.length]
 // }
 // console.log(temp);
+
+// left rotation 2 step best way
+
+// let arr = [1, 2, 3, 4, 5];
+// let k = Number(prompt("Enter k value"));
+// k= k%arr.length
+
+// reverse(0,k-1)
+// reverse(k,arr.length-1)
+// reverse(0,arr.length-1)
+
+// function reverse(i,j){
+//   while(i<j){
+//       let temp = arr[i]
+//     arr[i]= arr[j]
+//     arr[j]=temp
+//     i++
+//     j--
+//   }
+// }
+// console.log(arr);
+
+
+
