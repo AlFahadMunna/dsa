4898e9ae343844ff65fbdc7f11c2e35b818572a2
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-06-07 00:12:10 +0600

    some changes added

--- sorting_algorithms/app.js
+++ sorting_algorithms/app.js
@@ -1,38 +1,38 @@
-// Bubble sort
+Bubble sort
 
-// let arr = [10,5,12,1,3]
-// let n = arr.length;
+let arr = [10,5,12,1,3]
+let n = arr.length;
 
-// for(let i=0;i<n-1;i++){
-//     for(j=0;j<n-1-i;j++){
-//         if(arr[j]>arr[j+1]){
-//             let temp = arr[j]
-//             arr[j]= arr[j+1]
-//             arr[j+1]=temp
-//         }
-//     }
-// }
-// console.log(arr);
+for(let i=0;i<n-1;i++){
+    for(j=0;j<n-1-i;j++){
+        if(arr[j]>arr[j+1]){
+            let temp = arr[j]
+            arr[j]= arr[j+1]
+            arr[j+1]=temp
+        }
+    }
+}
+console.log(arr);
 
-// selection sort
+selection sort
 
-// let arr = [10,5,12,1,3]
+let arr = [10,5,12,1,3]
 
-// let n = arr.length;
-// for(let i= 0;i<n-1;i++){
-//     let minIndex =i;
-//     for(let j =i+1;j < n;j++){
-//         if(arr[minIndex]> arr[j]){
-//             minIndex = j;
-//         }
-//     }
-//     if(minIndex != i){
-//         let temp =arr[minIndex]
-//         arr[minIndex]=arr[i]
-//         arr[i]=temp
-//     }
-// }
-// console.log(arr);
+let n = arr.length;
+for(let i= 0;i<n-1;i++){
+    let minIndex =i;
+    for(let j =i+1;j < n;j++){
+        if(arr[minIndex]> arr[j]){
+            minIndex = j;
+        }
+    }
+    if(minIndex != i){
+        let temp =arr[minIndex]
+        arr[minIndex]=arr[i]
+        arr[i]=temp
+    }
+}
+console.log(arr);
 
 insertion
 
