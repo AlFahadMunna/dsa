8498ac8a59b08bbad789eb334fcd10afc29e1ec8
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-06-08 22:23:42 +0600

    fibonacci n num of result

--- recursion/app.js
+++ recursion/app.js
@@ -22,3 +22,26 @@
 //   // User return when previous state wait for next calculation (with value)
 // }
 // console.log(fact(6));
+
+// Fibonacci
+// n numbers of fibonacci
+
+let n = 10
+let first =0; second =1;
+
+function fibonacci(n,first,second){
+    if(n==0) return
+    let third = first+second
+    process.stdout.write(third+" ")
+    fibonacci(n-1,second,third)
+}
+process.stdout.write(0+" "+1+" ")
+fibonacci(n-2,0,1)
+
+// Using for loop
+// for(let i=1;i<=n-2;i++){
+//     let third = first+second;
+//     first = second
+//     second = third
+//     process.stdout.write(third+" ")
+// }
