eba603f15cdc2bd1be5d53e67e5918eaa47a20ea
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-06-08 22:09:13 +0600

    recursion with factorial number

---
+++ recursion/app.js
@@ -0,0 +1,24 @@
+// function recur(n){
+//     if(n===0)return
+//     console.log(n);
+//     recur(n-1)
+// }
+// recur(5)
+
+
+// Backtracking 
+// function sum(n) {
+//   if (n == 1) return 1;
+//   return n + sum(n - 1);
+//   // User return when previous state wait for next calculation (with value)
+// }
+// console.log(sum(5));
+
+// Factorial
+
+// function fact(n) {
+//   if (n == 1) return 1;
+//   return n * fact(n - 1);
+//   // User return when previous state wait for next calculation (with value)
+// }
+// console.log(fact(6));
